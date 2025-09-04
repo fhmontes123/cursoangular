bc69306af82d5b156f8433b5643900a0562bba0f
hmontes <hugomontes927@gmail.com>
2025-09-03 20:54:43 -0400

    03. Hooks

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Saludo } from './saludo/saludo';
 import { MiComponente01 } from './mi-componente01/mi-componente01';
+import { MiComponente02 } from './mi-componente02/mi-componente02';
 
 @Component({
   selector: 'app-root',
-  imports: [Saludo, MiComponente01],
+  imports: [Saludo, MiComponente01, MiComponente02],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
