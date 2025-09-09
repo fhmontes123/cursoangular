a89e5951368afda57cc8469b6cabae0fb5195940
hmontes <hugomontes927@gmail.com>
2025-09-08 20:10:56 -0400

    08. Pipes Personalizado

--- src/app/mi-componente04/mi-componente04.ts
+++ src/app/mi-componente04/mi-componente04.ts
@@ -1,12 +1,13 @@
 import { CommonModule, registerLocaleData } from '@angular/common';
 import { Component, LOCALE_ID } from '@angular/core';
 import localeEsBo from '@angular/common/locales/es-BO';
+import { AgePipe } from '../pipes/age-pipe';
 
 registerLocaleData(localeEsBo);
 
 @Component({
   selector: 'app-mi-componente04',
-  imports: [CommonModule],
+  imports: [CommonModule, AgePipe],
   providers: [{ provide: LOCALE_ID, useValue: 'es-BO' }],
   templateUrl: './mi-componente04.html',
   styleUrl: './mi-componente04.css',
@@ -17,6 +18,7 @@ export class MiComponente04 {
   avancePorcentaje = 0.45;
   domicilio = { zona: 'Miraflores', calle: 'Av. America', nro: 123 };
   myDate = new Date();
+  myBirthday = new Date('2000-05-15');
 
   myPromise = new Promise((resolve) => {
     setTimeout(() => {
