8272209c004eeadd3963be9bc071ce52cd18cb29
hmontes <hugomontes927@gmail.com>
2025-09-10 19:53:16 -0400

    11. DOM > Renderer2

--- src/app/mi-componente07/mi-componente07.ts
+++ src/app/mi-componente07/mi-componente07.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HighlightDirective } from '../directives/highlight.directive';
+import { HighlightGreenDirective } from '../directives/highlight-green.directive';
 
 @Component({
   selector: 'app-mi-componente07',
-  imports: [HighlightDirective],
+  imports: [HighlightDirective, HighlightGreenDirective],
   templateUrl: './mi-componente07.html',
   styleUrl: './mi-componente07.css',
 })
