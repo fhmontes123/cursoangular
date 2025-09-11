17b8093fa5b55ee400336bc4bf5e4d1894ccf52d
hmontes <hugomontes927@gmail.com>
2025-09-10 20:04:02 -0400

    11. DOM > HostListener

--- src/app/mi-componente07/mi-componente07.ts
+++ src/app/mi-componente07/mi-componente07.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HighlightDirective } from '../directives/highlight.directive';
 import { HighlightGreenDirective } from '../directives/highlight-green.directive';
+import { HighlightHoverDirective } from '../directives/highlight-hover.directive';
 
 @Component({
   selector: 'app-mi-componente07',
-  imports: [HighlightDirective, HighlightGreenDirective],
+  imports: [HighlightDirective, HighlightGreenDirective, HighlightHoverDirective],
   templateUrl: './mi-componente07.html',
   styleUrl: './mi-componente07.css',
 })
