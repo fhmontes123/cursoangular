817a08dc52fd15d916976afc617ad38ae9e27aa0
hmontes <hugomontes927@gmail.com>
2025-09-15 20:18:57 -0400

    21. Formulario Reactivo > FormGroup

--- src/app/components/user/new-user/new-user.ts
+++ src/app/components/user/new-user/new-user.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -11,4 +11,12 @@ import { MatInput } from '@angular/material/input';
   templateUrl: './new-user.html',
   styleUrl: './new-user.css',
 })
-export class NewUser {}
+export class NewUser {
+  formGroup = new FormGroup({
+    nombres: new FormControl(''),
+  });
+
+  clickRegister(): void {
+    console.log(this.formGroup.get('nombres')?.value);
+  }
+}
