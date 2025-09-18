2e8e8cddbf4bdd9f7bef31e31366a94744c750c1
hmontes <hugomontes927@gmail.com>
2025-09-17 21:06:41 -0400

    30. Service > Uso de un servicio

--- src/app/components/product/products/products.ts
+++ src/app/components/product/products/products.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductsApiService } from '../../../services/products-api.service';
 
 @Component({
   selector: 'app-products',
   imports: [],
   templateUrl: './products.html',
-  styleUrl: './products.css'
+  styleUrl: './products.css',
 })
 export class Products {
-
+  private readonly _productsApiService = inject(ProductsApiService);
 }
