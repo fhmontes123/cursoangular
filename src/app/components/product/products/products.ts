572751a05b5ada2d0ebcdb635d5156128808d4e2
hmontes <hugomontes927@gmail.com>
2025-09-19 20:02:47 -0400

    32. Listar Productos Plantilla > Observables

--- src/app/components/product/products/products.ts
+++ src/app/components/product/products/products.ts
@@ -1,10 +1,13 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { ProductsApiService } from '../../../services/products-api.service';
 import { Subscription } from 'rxjs';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { Product } from '../../../services/models/product.interface';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [MatSidenavModule, MatCardModule],
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
@@ -12,10 +15,16 @@ export class Products implements OnInit, OnDestroy {
   private readonly _productsApiService = inject(ProductsApiService);
   private _subcription!: Subscription;
 
+  products: Product[] = [];
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
   ngOnInit(): void {
-    this._subcription = this._productsApiService
-      .getProducts()
-      .subscribe((data) => console.log(data));
+    this._subcription = this._productsApiService.getProducts().subscribe((data) => {
+      console.log(data);
+      this.products = data;
+      this.cdr.detectChanges();
+    });
   }
 
   ngOnDestroy(): void {
